dc738987a88d6818df62742a4605d93ac0676e87
Luis Felipe De La Rosa Marrera <luismarrero_10@hotmail.com>
2019-11-23 22:59:05 -0400

    Login View Model with Base View Model

--- LandsXamarin/LandsXamarin/ViewModels/LoginViewModel.cs
+++ LandsXamarin/LandsXamarin/ViewModels/LoginViewModel.cs
@@ -1,24 +1,94 @@
 ﻿namespace LandsXamarin.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
     using System.Windows.Input;
-    public class LoginViewModel
+    using Xamarin.Forms;
+
+    public class LoginViewModel : BaseViewModel
     {
+        #region Attributes
+        private string password;
+        private bool isRunning;
+        private bool isEnabled;
+        #endregion
+
         #region Properties
         public string Email { get; set; }
-        public string Password { get; set; }
-        public bool IsRunning { get; set; }
+        public string Password
+        {
+            get { return this.password; }
+            set { SetValue(ref this.password, value); }
+        }
+        public bool IsRunning
+        {
+            get { return this.isRunning; }
+            set { SetValue(ref this.isRunning, value); }
+        }
         public bool IsRemembered { get; set; }
+        public bool IsEnabled 
+        {
+            get { return this.isEnabled; }
+            set { SetValue(ref this.isEnabled, value); }
+        }
         #endregion
 
         #region Constructors
         public LoginViewModel()
         {
             this.IsRemembered = true;
+            this.IsEnabled = true;
         }
         #endregion
 
         #region Commands
-        public ICommand ValidateMeCommand { get; set; }
+        public ICommand ValidateMeCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Please enter a valid E-mail",
+                    "Accept");
+                return;
+            }
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Please enter a Password",
+                    "Accept");
+                return;
+            }
+            this.IsRunning = true;
+            this.IsEnabled = false;
+            if (this.Email != "ltest@gmail.com" || this.Password != "LTest1")
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Email or Password incorrect",
+                    "Accept");
+                this.Password = string.Empty;
+                return;
+            }
+            this.IsRunning = false;
+            this.IsEnabled = true;
+            await Application.Current.MainPage.DisplayAlert(
+                    "Ok",
+                    "Please stand by...",
+                    "Accept");
+            return;
+        }
         #endregion
     }
 }
